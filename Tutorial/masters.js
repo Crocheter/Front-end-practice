599f76c42e9256baa29d1c77d50938dbe7a7e2f3
Crocheter <abisolaodutayo@yahoo.com>
2022-03-22 20:56:01 -0700

    my changes

---
+++ Tutorial/masters.js
@@ -0,0 +1,19 @@
+var students = ['Abisola', 'Blessing', 'Abigail'];
+
+//for (let i = 0; i < students.length; i++) {
+  //  const element = students[i];
+    
+//}
+
+var students = [ "Abisola", "Blessing", "Abigail" ];
+
+while (students.length > 0) {
+    let student = students.shift();
+    console.log(`Hello, ${student}! `);    
+}
+
+var favouriteBook []
+
+function addFavouriteBook(bookName) {
+  
+}
